5d593349c8fc1608e062f8d23d91eedd478be13f
ZackMacTavish <zmactavish@gmail.com>
2020-06-30 20:38:43 -0400

    added responsive hamburger menu

--- Zack.js
+++ Zack.js
@@ -1,37 +1,28 @@
 ﻿//Menu First ID
 
-$('#1').click(function () {
+$('#Home').click(function () {
     location.href = "index.html";
-});
-
-$('.1').click(function(){
-    $('1').trigger('click'); // will behave as if #1 is clicked.
 });
 
 
 //Menu Second ID
 
-$('#2').click(function () {
+$('#Project').click(function () {
     location.href = "Mywork.html";
 });
 
-$('.2').click(function(){
-    $('2').trigger('click'); // will behave as if #2 is clicked.
-});
 
-$('#3').click(function () {
+$('#About').click(function () {
     location.href = "About.html"; // will behave as if #3 is clicked.
 });
 
-$('#4').click(function () {
+$('#Contact').click(function () {
     location.href = "Contact.html"; // will behave as if #3 is clicked.
 });
 //Menu Second ID
 
-
-//Open Menu Click
-
 $('button').on('click', function(){
-  $('body').toggleClass('open');
-});
+    $('body').toggleClass('open');
+  });
+
 
